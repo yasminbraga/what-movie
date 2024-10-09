cf5c927deb260ecdd889f86c9f38a3ef41d6a4c5
Yasmin Braga <yasminbragateixeira@gmail.com>
2024-10-09 15:43:51 -0300

    fix btn layout

--- src/components/iconBtn.tsx
+++ src/components/iconBtn.tsx
@@ -9,12 +9,12 @@ const IconBtn: React.FC<ButtonProps> = ({ name, icon, ...props }) => {
   return (
     <button
       {...props}
-      className={`bg-white rounded-full py-4 px-8 flex items-center gap-2 uppercase font-semibold ${
+      className={`bg-white rounded-full py-5 px-6 flex items-center gap-2 uppercase font-semibold ${
         !name.includes("não") ? "text-red-500" : "text-gray-800"
       }`}
     >
       <Image src={`/${icon}`} alt={name} width={30} height={30} />
-      {name}
+      <span className="hidden lg:block">{name}</span>
     </button>
   );
 };
