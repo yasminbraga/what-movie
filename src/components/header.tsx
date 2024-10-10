e576e32241c8afbfab5d96f4252d78ab2ae41486
Yasmin Braga <yasminbragateixeira@gmail.com>
2024-10-10 15:39:57 -0300

    add sidebar menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,8 +8,11 @@ const Header: React.FC = () => {
 
   return (
     <nav className="w-full bg-gradient-to-r from-[#ff5656] to-[#ff1c1c]">
-      <div className="flex items-center justify-between px-6">
-        <button onClick={() => setMenuIsOpen(!menuIsOpen)}>
+      <div className="flex items-center justify-between lg:justify-center px-6">
+        <button
+          onClick={() => setMenuIsOpen(!menuIsOpen)}
+          className="lg:hidden"
+        >
           <Image
             src={"/menu-lateral.png"}
             alt="icone de menu lateral"
--- src/components/mainContainer.tsx
+++ src/components/mainContainer.tsx
@@ -10,7 +10,7 @@ const MainContainer = ({
   const { menuIsOpen } = useData();
   return (
     <div
-      className={`flex flex-col absolute duration-200 ${
+      className={`flex flex-col absolute duration-200 w-full ${
         menuIsOpen ? "left-[250px]" : "left-0"
       } `}
     >
